2ef286d76ad76ff977ece9756e193f5c3021b5c3
rwbogl <robert.w.bliss@gmail.com>
2017-09-23 19:43:10 -0400

    Escape math in docstring to parse correctly

--- pisot.py
+++ pisot.py
@@ -24,19 +24,17 @@ from sympy.series.sequences import SeqBase
 import cfinite
 
 class Pisot(SeqBase):
-
     """
     This class defines basic methods for dealing with Pisot sequences.
 
     The Pisot sequence :math:`E_r(x, y)` is defined by
 
     .. math::
-
-        \begin{align*}
-            a_0 &= x \\
-            a_1 &= y \\
-            a_{n + 1} &= floor(a_{n - 1}**2 / a_{n - 2} + r),
-        \end{align*}
+        \\begin{align}
+            a_0 &= x \\\\
+            a_1 &= y \\\\
+            a_n &= \\left\lfloor \\frac{a_{n - 1}^2}{a_{n - 2}} + r \\right\\rfloor,
+        \\end{align}
 
     where :math:`0 < x < y` are integers, and :math:`r` is some constant.
     """
